95d2afd7fb04af4f3f107b68cc780a1a48c6a138
dingmandeng <makemake1293@gmail.com>
2021-11-10 01:56:23 -0800

    aside

---
+++ src/components/Aside.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <el-menu
+        style="width: 200px; min-height: calc(100vh - 50px)"
+        default-active="2"
+        class="el-menu-vertical-demo">
+      <!--        @open="handleOpen"
+              @close="handleClose"-->
+
+
+      <el-sub-menu index="1-4">
+        <template #title>选项1</template>
+        <el-menu-item index="1-4-1">选项1-1</el-menu-item>
+      </el-sub-menu>
+
+
+      <el-menu-item index="2">
+        <el-icon><icon-menu /></el-icon>
+        <span>Navigator Two</span>
+      </el-menu-item>
+      <el-menu-item index="3" disabled>
+        <el-icon><document /></el-icon>
+        <span>Navigator Three</span>
+      </el-menu-item>
+      <el-menu-item index="4">
+        <el-icon><setting /></el-icon>
+        <span>Navigator Four</span>
+      </el-menu-item>
+    </el-menu>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Aside"
+}
+</script>
+
+<style scoped>
+
+</style>
